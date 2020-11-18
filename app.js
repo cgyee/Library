cc94c84ee7a16900df091c2a6ed9b7dbb716c23f
cgyee <cyee2017@fau.edu>
2020-11-17 22:18:58 -0500

    finished html and js rendered portion of html

--- app.js
+++ app.js
@@ -6,22 +6,44 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.index = 0;
+    
     this.info = function() {
-        return title + " by " + author + " is " + pages + " and  " + read;
+        return title + " is by " + author + " it is " + pages + " and you  " + this.returnTextIfRead();
     };
+    
     this.setIndex = function(number) {
         index = number;
     };
+    
     this.getIndex = function() {
         return index;
     };
+
+    this.getRead = function() {
+        return read;
+    }
+    
     this.toggleReadStatus = function() {
-        if(read == "has read") {
-            read = "has not read";
+        read = !read;
+        return read;
+
+    };
+    
+    this.getPages = function() {
+        return pages;
+    };
+    
+    this.returnTextIfRead = function() {
+        if(read) {
+            return "have read it!";
         }
         else {
-            read = "has read";
+            return "have not read!";
         }
+    };
+
+    this.getAuthor = function() {
+        return author;
     };
 }
 
@@ -45,9 +67,9 @@ function removeBookFromLibrary(index) {
 
 function viewCurrentLibrary() {
     const row = document.querySelector('.row');
-    while(row.firstChild) {
+    /* while(row.firstChild) {
         row.removeChild(row.firstChild);
-    }
+    } */
     myLibrary.forEach(book => {
         addBookToPage(book);
     });
@@ -58,46 +80,66 @@ function addBookToPage(book) {
     const row = document.querySelector('.row');
     bookDiv = document.createElement('div');
     cardDiv = document.createElement('div');
-    imgDiv = document.createElement('img');
-    rectangle = document.createElement('rect');
+    svgDiv = document.createElementNS('http://www.w3.org/2000/svg','svg');
+    textSvg = document.createElement('text');
+    rectSvg = document.createElementNS('http://www.w3.org/2000/svg','rect');
     cardBodyDiv = document.createElement('div');
     bookInfo = document.createElement('p');
     buttonsFlex = document.createElement('div');
     smallText = document.createElement('small');
     buttonGroup = document.createElement('div');
-    buttonHasRead = document.createElement('button');
+    // buttonAdd = document.createElement('button');
+    buttonRemove = document.createElement('button');
+    formCheck =  document.createElement('div');
+    checkbox = document.createElement('input');
+    checkboxLabel = document.createElement('label');
     
-    bookDiv.className = "col-sm-3";
+    bookDiv.className = "col-md-4";
     cardDiv.className = "card mb-4 shadow-sm";
-    imgDiv.className = "bd-placeholder-img card-img-top";
+    svgDiv.className = "bd-placeholder-img card-svg-img";
     cardBodyDiv.className = "card-body";
     bookInfo.className = "card-text";
     buttonsFlex.className = "d-flex justify-content-between align-items-center";
     buttonGroup.className = "btn-group";
-    buttonHasRead.className = "btn btn-sm btn-outline-secondary";
+    // buttonAdd.className = "btn btn-sm btn-outline-secondary";
+    buttonRemove.className = "btn btn-sm btn-outline-secondary";
+    formCheck.className = "form-check";
+    checkbox.className = "form-check-input";
+    checkboxLabel.className = "form-check-label";
 
-    imgDiv.setAttribute("src", "css/open-book.png")
-    imgDiv.setAttribute("width", "100%");
-    imgDiv.setAttribute("height", "255");
-    rectangle.setAttribute("width", "100%");
-    rectangle.setAttribute("height", "100%");
-    rectangle.setAttribute("fill", "#55595c");
-    
-    bookInfo.textContent = book.info();
-    buttonHasRead.textContent = book.read;
-    smallText.textContent = book.pages;
+    //svgDiv.setAttribute("src", "css/open-book.png")
+    svgDiv.setAttribute("width", "100%");
+    svgDiv.setAttribute("height", "255");
+    // svgDiv.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    rectSvg.setAttribute("width", "100%");
+    rectSvg.setAttribute("height", "100%");
+    rectSvg.setAttribute("fill", "#55595c");
 
-    buttonGroup.append(buttonHasRead);
-    buttonsFlex.append(buttonGroup, smallText);
-    //bookInfo.append(buttonsFlex);
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.checked = book.getRead();
+    checkboxLabel.textContent = "You " + book.returnTextIfRead();
+
+    bookInfo.textContent = "Written by: " + book.getAuthor();
+    // buttonAdd.textContent = "Add";
+    buttonRemove.textContent = "Del";
+    smallText.textContent = "Pages: " + book.getPages();
+
+    formCheck.append(checkbox, checkboxLabel);
+    buttonGroup.append(buttonRemove);
+    buttonsFlex.append(buttonGroup, formCheck, smallText);
     cardBodyDiv.append(bookInfo, buttonsFlex);
-    imgDiv.append(rectangle);
-    cardDiv.append(imgDiv, cardBodyDiv);
-    //cardDiv.append(imgDiv);
+    rectSvg.append(textSvg);
+    svgDiv.append(rectSvg);
+    cardDiv.append(svgDiv, cardBodyDiv);
     bookDiv.append(cardDiv);
     row.append(bookDiv);
 }
 
-const testBook = new Book("Lord of the Things", "Some Guy", "many", "has not");
+function toggleReadTextButton() {
+
+}
+
+const testBook = new Book("Lord of the Things", "Some Guy", "many", true);
 addBookToLibrary(testBook);
+viewCurrentLibrary();
 console.log(myLibrary);
